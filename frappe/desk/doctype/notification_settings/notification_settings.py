a25f2a5dfc278c41535455cbc35f223a841b29a4
Mahesh <mahesh@sodelsolutions.com>
2021-06-23 15:19:01 +0530

    fix notification type error

--- frappe/desk/doctype/notification_settings/notification_settings.py
+++ frappe/desk/doctype/notification_settings/notification_settings.py
@@ -28,7 +28,7 @@ def is_email_notifications_enabled_for_type(user, notification_type):
 	if not is_email_notifications_enabled(user):
 		return False
 
-	if notification_type == 'Alert':
+	if notification_type == 'Alert' or notification_type == 'WORKFLOWACTION':
 		return False
 
 	fieldname = 'enable_email_' + frappe.scrub(notification_type)
